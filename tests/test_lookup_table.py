e7b2663f1ccc6e81364b6484eb35297dbbf36f00
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-06-19 12:57:40 -0700

    remove tests

--- tests/test_lookup_table.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from lidarpf.utils.lookup_table import compute_lookup_table
-
-
-def test_compute_lookup_table_shape():
-    occupancy_grid = np.zeros((5, 5), dtype=bool)
-    num_angles = 8
-    table = compute_lookup_table(occupancy_grid, num_angles)
-    assert table.shape == (5, 5, 8)
